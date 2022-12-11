6daa496a8cf2ad34b9958e07f7e429e72c6e1a23
yahel dooley <ydooley@gmail.com>
2022-12-10 22:23:31 -0800

    Completed JS Exercises 1-5

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = undefined;
+let lastName = "Dooley";
+var age = 38;
 
 // Exercise 2
+firstName = "Yahel";
+lastName = "Dooley";
+age = 38;
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = "1995";
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pagecount = 722;
+let bookmark = 456;
+let hasRead = true;
